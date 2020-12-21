0b4507f1d4bb94f498f0d17fe761cbf3eb1bd3d8
sat2493 <satabligan@ucdavis.edu>
2020-12-20 18:19:05 -0800

    Update Neural Networks.py
    Extended neural net with an update functionality for every single training example

--- Neural Networks.py
+++ Neural Networks.py
@@ -1,30 +1,51 @@
 class Network:
-    def __init__(self, x, batch_num, numOutputNeurons):
-        # Number of Neurons in Each Layer
-        self.numOutputNeurons = numOutputNeurons
+    def __init__(self, numNeuronsInEachLayers):
         
-        # Initialize Input & Output Layers
+        # Initialize Input Layer
+        self.inputlayer = self.InputLayer(numNeuronsInEachLayer[0])
+        self.layers = [self.inputlayer]
         
-        self.inputlayer = self.InputLayer(x[batch_num])
+        # Initialize each succeeding layer based on the one before it
         
-        # Initialize succeeding layers, up until the y layer is reached
-        # Add Hidden Layers in between
-        self.outputlayer = self.NextLayer(self.numOutputNeurons, x[batch_num].size)
+        # Have a counter to keep track of which layer we are at
+        count = 0
+        for numNeurons in numNeuronsInEachLayer:
+            # Skip the input layer, which has no preceding layer
+            if count == 0:
+                count = count + 1
+                continue
+            nextlayer = self.NextLayer(numNeurons, numNeuronsInEachLayer[count - 1])
+            self.layers.append(nextlayer)
+            count = count + 1
+            
+    def update(self, x):
         
-    def calculate_activation(self, prevLayer, currLayer):
+        count = 0
+        for layer in self.layers:
+            # Read inputs onto input layer
+            if layer == self.inputlayer:
+                layer.read(x)
+            # Update other layers based on the neurons from the previous layer
+            else:
+                layer = self.calculate_activations(self.layers[count - 1], layer)
+            count = count + 1
+        
+    def calculate_activations(self, prevLayer, currLayer):
         # Matrix-vector multiplication to calculate next layer values
         currLayer.neurons = np.matmul(currLayer.weights, prevLayer.neurons)
         # Subtract Current Layer's Biases from Neurons
-        currLayer.neurons = currLayer.neurons - currLayer.biases
-        # Apply the sigmoid algorithm to the result
-        
+        currLayer.neurons = currLayer.neurons 
+        # Apply the sigmoid function to the result
+        currLayer.neurons = 1 / ( np.ones(currLayer.neurons.size) + pow(np.e, -currLayer.neurons) )
         
     def decision(self):
-        neuron_activation = self.outputlayer.neurons[0]
+        lastlevel = len(self.layers) - 1
+        outputlayer = self.layers[lastlevel]
+        neuron_activation = outputlayer.neurons[0]
         max_number = 0
         number = 0
  
-        for i in self.outputlayer.neurons:
+        for i in outputlayer.neurons:
             if i > neuron_activation:
                 max_number = number
             number = number + 1
@@ -35,14 +56,15 @@ class Network:
     class InputLayer:
         # Read image pixels, initialize neurons with the values of each image
         
-        def __init__(self, x):
-            # self.neurons = np.ones(numNeurons)
-            # Initialize empty array
-            self.neurons = []
+        def __init__(self, numNeurons):
+            self.neurons = np.ones(numNeurons)
+            
+        def read(self, x):
+            count = 0
             for i in x:
                 for pixel in i:
-                    self.neurons.append(pixel)
-            self.neurons = np.array(self.neurons)
+                    self.neurons[count] = pixel
+                    count = count + 1
             
     class NextLayer: 
         def __init__(self, numCurrLayerNeurons, numPrevLayerNeurons):
