Fix bugs in derivative functions

Neural Networks.py
@@ -108,7 +108,7 @@ def derivative_of_cost_with_respect_to_bias(current_neuron, z_value, ideal_outpu
     return ((pow(np.e, -z_value)) / pow((1 + pow(np.e, -z_value)), 2)) * (2 * (current_neuron - ideal_output))
 
 # Finds derivative with respect to a single weight parameter
-def derivative_of_cost_with_respect_to_previous_neuron(current_neuron, weight, z_value, ideal_output):
+def derivative_of_cost_with_respect_to_previous_neuron(weight, current_neuron, z_value, ideal_output):
     return weight * ((pow(np.e, -z_value)) / pow((1 + pow(np.e, -z_value)), 2)) * (2 * (current_neuron - ideal_output))
 
 def partial_backpropagation(currLayer, prevLayer, ideal_outputs):